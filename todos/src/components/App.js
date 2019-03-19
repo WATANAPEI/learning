e20111bb3ac3c111ff636e592e878c9b48d9ef51
WATANAPEI <pei2791150@gmail.com>
2019-03-19 18:38:56 +0200

    change footer appearance

--- todos/src/components/App.js
+++ todos/src/components/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import LoadText from '../containers/LoadText'
-import NewQuote from '../containers/NewQuote'
-import TweetQuote from './TweetQuote'
 import HeaderBar from './HeaderBar'
 
 const App = () => (
   <div id="quote-box">
     <HeaderBar />
     <LoadText />
-    <NewQuote />
-    <TweetQuote />
   </div>
 )
 
