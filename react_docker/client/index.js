38321b79e84d55e8fe9aa64ed170c8312eb2a49c
WATANAPEI <pei2791150@gmail.com>
2019-04-04 20:57:40 +0200

    add some files

---
+++ react_docker/client/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import request from 'superagent';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+        };
+    }
+
+    componentDidMount() {
+        request.get('/api/items').end((err, res) => {
+            this.setState({
+                items: res.body,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Docker Compose Test</h1>
+                <ul>
+                    { this.state.items.map((item, index) => <li key={index}>{item}</li>)}
+                </ul>
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
