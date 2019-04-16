62271a313efeb39f7effbbe53923040a5674509d
WATANAPEI <pei2791150@gmail.com>
2019-04-16 20:40:36 +0200

    include html

--- auto-quote/webpack.config.js
+++ auto-quote/webpack.config.js
@@ -1,32 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
-        js: "./client/index.js",
-        html: "./client/index.html"
+        js: "./client/index.js"
     },
     output: {
         path: __dirname + "/out",
         filename: 'bundle.js'
     },
     module: {
-        rules: [
-        {
-            test: /\.js$/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                      presets: ['env', 'react']
-                    }
-                },
-            ],
-            exclude: /node_modules/
-        },
-        {
-            test: /\.html$/,
-            use: ['file?name=[name].[ext]'],
-            exclude: /node_modules/
-        }
-    ],
- },
+            rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                          presets: ['env', 'react']
+                        }
+                    },
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: ['file?name=[name].[ext]'],
+                exclude: /node_modules/
+            }
+        ],
+     },
+    plugins: [
+      new HtmlWebpackPlugin({template: './client/index.html'})
+    ]
 };
 
