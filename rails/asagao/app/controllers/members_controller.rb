32a7399e67986e150f7b6a662528b2fabc61dfab
WATANAPEI <pei2791150@gmail.com>
2019-05-22 21:11:44 +0200

    reached 13.4

--- rails/asagao/app/controllers/members_controller.rb
+++ rails/asagao/app/controllers/members_controller.rb
@@ -44,6 +44,8 @@ class MembersController < ApplicationController
   end
   private def member_params
     attrs = [
+      :new_profile_picture,
+      :remove_profile_picture, 
       :number,
       :name,
       :full_name,
