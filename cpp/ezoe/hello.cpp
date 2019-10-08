cbdb4e39e0319518de703b1a2b5b355bf3a1fd55
WATANAPEI <info@wpei.dev>
2019-10-08 23:36:30 +0900

    add c++ files

---
+++ cpp/ezoe/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Integer:"s << 42 << "\n"s
+        << "Floating Point"s << 3.14 ;
+}
---
+++ cpp/ezoe/main.cpp
@@ -0,0 +1,7 @@
+double plus( double x, double y) {
+    return x + y;
+}
+int main() {
+    std::cout << plus(1, 3);
+}
+
