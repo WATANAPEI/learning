6fde259af051e5a120047308d3d6a6b969353265
WATANAPEI <info@wpei.dev>
2019-10-08 23:38:42 +0900

    remove cpp files

--- cpp/ezoe/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Integer:"s << 42 << "\n"s
-        << "Floating Point"s << 3.14 ;
-}
--- cpp/ezoe/main.cpp
+++
@@ -1,7 +0,0 @@
-double plus( double x, double y) {
-    return x + y;
-}
-int main() {
-    std::cout << plus(1, 3);
-}
-
