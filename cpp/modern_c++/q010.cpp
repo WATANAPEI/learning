13a374844b73d8fec32de3ba6f35ee5e7833967d
WATANAPEI <info@wpei.dev>
2019-10-12 21:33:19 +0900

    solved question 010

--- cpp/modern_c++/q010.cpp
+++ cpp/modern_c++/q010.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 #include "mylib.h"
 #include <bitset>
+#include <cmath>
 
 using namespace std;
 
 template <size_t N>
-std::bitset<N> calc_greycode(std::bitset<N> b) {
+unsigned int calc_decimal(bitset<N> b) {
+    unsigned int result = 0;
+    for(unsigned int i = 1; i <= b.size(); i++) {
+        result += b[i] * pow(2, i-1);
+    }
+    return result;
+}
+
+template <size_t N>
+bitset<N> calc_greycode(bitset<N> b) {
     return b ^ (b >> 1);
 
 }
+
+template <size_t N>
+bitset<N> calc_binary(bitset<N> g) {
+    bitset<N> b(N);
+    // 1 digit
+    b[N-1] = g[N-1];
+    //cout << "g[N-1] : " << g[N-1] << endl;
+    // 2 or more digit
+    for(int i = N - 2; i >= 0; --i) {
+        //cout << "i: " << i << " ";
+        //cout << "b[i]: " << b[i] << ", b[i+1]: " << b[i+1] << ", g[i]: " << g[i] << endl;
+        b[i] = b[i+1] ^ g[i];
+    }
+    return b;
+}
+
 
 int main() {
     const size_t upper_digit = 5;
@@ -16,8 +42,8 @@ int main() {
 
     size_t i = 0;
     while(!bit(i).all()) {
-        cout << i << ":\t " <<  bit(i) << " / " << calc_greycode(bit(i)) << endl;
+        cout << i << ":\t " <<  bit(i) << " / " << calc_greycode(bit(i)) << "\t" << calc_binary(calc_greycode(bit(i))) << endl;
         ++i;
     }
-    cout << i << ":\t " <<  bit(i) << " / " << calc_greycode(bit(i)) << endl;
+    cout << i << ":\t " <<  bit(i) << " / " << calc_greycode(bit(i)) << "\t" << calc_binary(calc_greycode(bit(i))) << endl;
 }
