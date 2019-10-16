ab94675ebef72adcfdf5e3a258c79c7b3c58cb0d
WATANAPEI <info@wpei.dev>
2019-10-16 23:23:19 +0900

    add example answer for question 16

--- cpp/modern_c++/q016.cpp
+++ cpp/modern_c++/q016.cpp
@@ -58,15 +58,53 @@ public:
             is.setstate(std::ios_base::failbit);
         return is;
     }
+
+    ipv4& operator++() {
+        *this = ipv4(1 + to_ulong());
+        return *this;
+    }
+
+    ipv4 operator++(int) {
+        ipv4 result(*this);
+        ++(*this);
+        return result;
+    }
+
+    friend bool operator==(ipv4 const & a1, ipv4 const & a2) noexcept {
+        return a1.data == a2.data;
+    }
+
+    friend bool operator!=(ipv4 const & a1, ipv4 const &a2) noexcept {
+        return !(a1 == a2);
+    }
+
+    friend bool operator<(ipv4 const & a1, ipv4 const & a2) noexcept {
+        return a1.to_ulong() < a2.to_ulong();
+    }
+
+    friend bool operator>(ipv4 const & a1, ipv4 const & a2) noexcept {
+        return a2 < a1;
+    }
+
+    friend bool operator<=(ipv4 const & a1, ipv4 const & a2) noexcept {
+        return !(a1 > a2);
+    }
+
+    friend bool operator>=(ipv4 const & a1, ipv4 const & a2) noexcept {
+        return !(a1 < a2);
+    }
+
 };
 
 int main() {
-    ipv4 address(168, 192, 0, 1);
-    std::cout << address << std::endl;
-
-    ipv4 ip;
-    std::cout << ip << std::endl;
-    std::cin >> ip;
-    if(!std::cin.fail())
-        std::cout << ip << std::endl;
+    std::cout << "input range: ";
+    ipv4 a1, a2;
+    std::cin >> a1 >> a2;
+    if (a2 > a1) {
+        for (ipv4 a = a1; a <= a2; a++) {
+            std::cout << a << std::endl;
+        }
+    } else {
+        std::cerr << "invalid range!: " << std::endl;
+    }
 }
