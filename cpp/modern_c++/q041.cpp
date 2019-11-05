f20839e07fd986e332df12fafc93ada8b0b3fe25
WATANAPEI <info@wpei.dev>
2019-11-05 23:40:48 +0900

    couldn't solve question 41

---
+++ cpp/modern_c++/q041.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "/usr/local/src/vcpkg/packages/date_x64-linux/include/date/iso_week.h"
+
+unsigned int week_day(int const y, unsigned int const m, unsigned int const d ) {
+    using namespace date;
+
+    if (m < 1 || m > 12 || d < 1 || d > 31) return 0;
+
+    auto const dt = date::year_month_day { year{y} , month{m}, day{d}};
+    auto const tiso = iso_week::year_weeknum_weekday{ dt };
+
+    return static_cast<unsigned int> (tiso.weekday());
+}
+
+int main () {
+    auto wday = week_day(2018, 5, 2);
+
+}
