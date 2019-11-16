c7a5394251f510ac2755f6c14f631a469c6dd87a
WATANAPEI <info@wpei.dev>
2019-11-16 16:36:00 +0900

    add answer for question 62

--- cpp/modern_c++/q062.cpp
+++ cpp/modern_c++/q062.cpp
@@ -1,7 +1,11 @@
+// use command below to compile
+// g++ -pthread -std=c++1z q062.cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <thread>
+#include <random>
+#include <chrono>
 
 using namespace std;
 
@@ -18,18 +22,18 @@ pair<T, T> pminmax(vector<T> const & v) {
     vector<T> min;
     vector<T> max;
     size_t n_thread = 10;
-    vector<thread> threads(n_thread);
+    vector<thread> threads;
     auto first = begin(v);
     auto last = first;
-    for(int i = 0; i < n_thread; i++) {
+    for(size_t i = 0; i < n_thread; i++) {
         first = last;
         last = (i == n_thread-1)? end(v): first + v.size() / n_thread;
-        threads.emplace_back([first, last, min, max]() {
+        threads.emplace_back([first, last, &min, &max]() {
                 min.push_back(*min_element(first, last));
                 max.push_back(*max_element(first, last));
                         });
     }
-    for(int i = 0; i < n_thread; i++) {
+    for(size_t i = 0; i < n_thread; i++) {
         threads[i].join();
     }
     return make_pair(*min_element(min.begin(), min.end()), *max_element(max.begin(), max.end()));
@@ -37,10 +41,29 @@ pair<T, T> pminmax(vector<T> const & v) {
 
 
 int main() {
-    using type = double;
+    const size_t count = 1'000'000;
+    using type = int;
+    vector<type> data(count);
+
+    random_device rd;
+    mt19937 mt;
+    auto seed_data = array<int, mt19937::state_size>{};
+    generate(begin(seed_data), end(seed_data), ref(rd));
+    seed_seq seq(begin(seed_data), end(seed_data));
+    mt.seed(seq);
+    uniform_int_distribution<> ud(1, 100);
+
+    generate_n(begin(data), count, [&mt, &ud]() {
+            return ud(mt);
+            });
+
     //vector<type> v1{1, 4, 2, 12, 5, 9, 0, -3, 4,5};
-    vector<type> v2{1.3, 4.0, 0.2, 0.12, -5.2, 9.9, 0.0, -3, 4.2,5.9};
-    pair<type, type> result = pminmax(v2);
+    //vector<type> v2{1.3, 4.0, 0.2, 0.12, -5.2, 9.9, 0.0, -3, 4.2,5.9};
+    auto start = chrono::system_clock::now();
+    pair<type, type> result = pminmax(data);
+    auto end = chrono::system_clock::now();
+    auto t1 = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "time: " << t1.count() << "ms" << endl;
     cout << "min: " << result.first << ", max: " << result.second << endl;
 
 
