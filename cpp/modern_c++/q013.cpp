9895585d8c314e7a9901b364336d34a47af189f4
WATANAPEI <info@wpei.dev>
2019-11-17 22:05:20 +0900

    copied answer for question 13

---
+++ cpp/modern_c++/q013.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <array>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+template <typename E = mt19937, typename D = uniform_real_distribution<>>
+double compute_pi(E& engine, D& dist, int const samples = 1000000) {
+    auto hit = 0;
+    for (auto i = 0; i < samples; i++) {
+        auto x = dist(engine);
+        auto y = dist(engine);
+        if (y <= sqrt(1 - pow(x, 2))) {
+            hit++;
+        }
+    }
+    return 4.0 * hit / samples;
+}
+
+int main() {
+    random_device rd;
+    auto seed_data = array<int , mt19937::state_size> {};
+    generate(begin(seed_data), end(seed_data), ref(rd));
+    seed_seq seq(cbegin(seed_data), cend(seed_data));
+    auto eng = mt19937{ seq };
+    auto dist = uniform_real_distribution<> { 0, 1};
+
+    for(auto j = 0; j < 10; j++) {
+        cout << compute_pi(eng, dist) << endl;
+    }
+
+}
+
