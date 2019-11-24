7338011a5a4eeabaa6595a94fd965e0cc5fe0882
WATANAPEI <info@wpei.dev>
2019-11-24 11:40:19 +0900

    solved question 42

---
+++ cpp/modern_c++/q042.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "/usr/local/src/vcpkg/packages/date_x64-linux/include/date/iso_week.h"
+#include "/usr/local/src/vcpkg/packages/date_x64-linux/include/date/date.h"
+
+using namespace std;
+using namespace date;
+
+int main() {
+    int y;
+    unsigned int m, d;
+    cout << "input year(yyyy):";
+    cin >> y;
+    cout << "input month(mm):";
+    cin >> m;
+    cout << "input day(dd):";
+    cin >> d;
+    if (m < 1 || m > 12 || d < 1|| d > 31) return 0;
+    auto const dt = date::sys_days { year{y}/ month{m}/ day{d}};
+    auto const s_dt = date::sys_days {year{y}/ month{1}/ day{1}};
+    auto const ymd = iso_week::year_weeknum_weekday(dt);
+    cout << dt << endl;
+    cout << s_dt << endl;
+    cout << "day_" << (dt - s_dt).count() + 1 << endl;
+    cout << "week_" << static_cast<unsigned int>(ymd.weeknum()) << endl;
+
+}
