6363e48eec626ef2d2d104f1be47337a262cb781
WATANAPEI <info@wpei.dev>
2019-12-08 22:38:12 +0900

    copied answer for q68

--- cpp/modern_c++/q068.cpp
+++ cpp/modern_c++/q068.cpp
@@ -1,81 +1,123 @@
 #include <iostream>
 #include <cctype>
-#include "q068.h"
 #include <memory>
+#include <array>
+#include <random>
+#include <algorithm>
 
-class LengthValidator::Impl {
+using namespace std;
+
+class password_generator{
 public:
-    unsigned int mLength;
+    virtual string generate() = 0;
+    virtual string allowed_chars() const = 0;
+    virtual size_t length() const = 0;
+    virtual void add(unique_ptr<password_generator> genarator) = 0;
+    virtual ~password_generator() = default;
+
 };
 
-LengthValidator::LengthValidator(int length = 10) : mImpl(new LengthValidator::Impl())
+class basic_password_generator : public password_generator{
+    size_t const len;
+public:
+    explicit basic_password_generator(size_t const len) noexcept : len(len) {}
+    virtual string generate() override{
+        throw runtime_error("Not implemented");
+    }
+    virtual void add(unique_ptr<password_generator>) override{
+        throw runtime_error("Not implemented");
+    }
+    virtual size_t length() const noexcept override final {
+        return len;
+    }
+};
+
+class digit_generator : public basic_password_generator
 {
-    mImpl->mLength = length;
+public:
+    explicit digit_generator(size_t const len) noexcept : basic_password_generator(len) {
+    }
+
+    virtual string allowed_chars() const override {
+        return "0123456789";
+    }
 };
 
-LengthValidator::~LengthValidator()
+class symbol_generator : public basic_password_generator
 {
-    std::cout << "Destracted" << std::endl;
-    //delete mImpl;
-    //mImpl = NULL;
+public:
+    explicit symbol_generator(size_t const len) noexcept
+    : basic_password_generator(len){}
+    virtual string allowed_chars() const override {
+        return "!@#$%^&*(){}[]<>?";
+    }
 };
 
-unsigned int LengthValidator::getLength() const
+class upper_letter_generator : public basic_password_generator
 {
-    return mImpl->mLength;
-};
-
-LengthValidator::LengthValidator(const LengthValidator &lengthValidator) : mImpl(new LengthValidator::Impl()) {
-    mImpl->mLength = lengthValidator.getLength();
-};
-
-LengthValidator &LengthValidator::operator=(const LengthValidator &lengthValidator) {
-    if(this != &lengthValidator) {
-        //mImpl.release();
-        mImpl->mLength = lengthValidator.getLength();
+public:
+    explicit upper_letter_generator(size_t const len) noexcept
+    : basic_password_generator(len) {}
+    virtual string allowed_chars() const override {
+        return "QBCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-    return *this;
 };
 
-bool LengthValidator::validate(const std::string &str) const {
-    return str.length() > mImpl->mLength;
+class lower_letter_generator : public basic_password_generator
+{
+public:
+    explicit lower_letter_generator(size_t const len) noexcept
+     : basic_password_generator(len) {};
+     virtual string allowed_chars() const override{
+         return "abcdefghijklmnopqrstuvwxyz";
+     }
 };
 
-ValidateDecorator::ValidateDecorator(std::unique_ptr<Validatable> validator) : inner(std::move(validator)) {
-};
-
-bool ValidateDecorator::validate(const std::string &str) const {
-    return inner->validate(str);
-};
-
-bool SymbolValidator::validate(const std::string &str) const {
-    for(auto e: str) {
-        if(ispunct(e)) return true;
+class composite_password_generator : public password_generator
+{
+    virtual string allowed_chars() const override {
+        throw runtime_error("not implemented");
     }
-    return false;
-};
-
-bool NumberValidator::validate(const std::string &str) const {
-    for(auto e: str) {
-        if(isdigit(e)) return true;
+    virtual size_t length() const override {
+        throw runtime_error("not implemented");
     }
-    return false;
-};
-
-
-bool CapitalValidator::validate(const std::string &str) const {
-    for(auto e: str) {
-        if(isupper(e)) return true;
+    random_device rd;
+    mt19937 eng;
+    vector<unique_ptr<password_generator>> generators;
+public:
+    composite_password_generator()
+    {
+        auto seed_data = array<int, mt19937::state_size> {};
+        std::generate(begin(seed_data), end(seed_data), ref(rd));
+        seed_seq seq(cbegin(seed_data), cend(seed_data));
+        eng.seed(seq);
     }
-    return false;
+    virtual string generate() override {
+        string password;
+        for (auto & generator : generators){
+            string chars = generator->allowed_chars();
+            uniform_int_distribution<> ud(0, static_cast<int>(chars.length() - 1));
+            for(size_t i = 0; i < generator->length(); ++i){
+                password += chars[ud(eng)];
+            }
+        }
+
+        shuffle(begin(password), end(password), eng);
+        return password;
+    }
+    virtual void add(unique_ptr<password_generator> generator) override
+    {
+        generators.push_back(move(generator));
+    }
+
 };
 
 int main() {
-    std::string test("fewf8fdsaf");
-    LengthValidator lengthValidator(9);
-    LengthValidator lengthValidator10(lengthValidator);
-    LengthValidator lengthValidator11(11);
-    lengthValidator11 = lengthValidator10;
-    std::cout << std::boolalpha << lengthValidator.validate(test) << std::endl;
+    composite_password_generator generator;
+    generator.add(make_unique<symbol_generator>(2));
+    generator.add(make_unique<digit_generator>(2));
+    generator.add(make_unique<upper_letter_generator>(2));
+    generator.add(make_unique<lower_letter_generator>(4));
 
+    auto const password = generator.generate();
 }
