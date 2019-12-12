32d7088697e026b08534dde78a3188d5dfa1fe0c
WATANAPEI <info@wpei.dev>
2019-12-12 22:50:23 +0900

    proceed a little

--- cpp/modern_c++/q073.cpp
+++ cpp/modern_c++/q073.cpp
@@ -1,8 +1,24 @@
 #include <cereal/cereal.hpp>
 #include <cereal/archives/xml.hpp>
+#include <vector>
 
 using namespace std;
 
+struct Movie {
+    unsigned int id;
+    string title;
+    unsigned int year;
+    unsigned int screeningTime;
+    vector<string> directors;
+    vector<string> writers;
+    vector<string> cast;
+    pair<string, string> castingRole;
+
+};
+
 int main() {
+    cereal::XMLOutputArchive archive(std::cout);
+    Movie movie{1, "Pokemon", 1999, 200, vector{"satoshi", "tsubasa"}, vector{"aa" "bb"}, vector{"deka"}}
+    archive( CEREAL_NVP(
 
 }
