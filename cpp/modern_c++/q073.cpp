288fdea62feb40223f85071b34cd7861c3729504
WATANAPEI <info@wpei.dev>
2019-12-14 18:23:37 +0900

    in the end the name of chile name couldn't be changed

--- cpp/modern_c++/q073.cpp
+++ cpp/modern_c++/q073.cpp
@@ -1,22 +1,51 @@
 #include <cereal/cereal.hpp>
 #include <cereal/archives/xml.hpp>
+#include <cereal/archives/json.hpp>
 #include <vector>
 #include <tuple>
 #include <sstream>
 #include <cereal/types/vector.hpp>
 #include <cereal/types/tuple.hpp>
+#include <cereal/details/helpers.hpp>
+#define TO_STRING(VariableName) # VariableName
 
 using namespace std;
 
+struct Role {
+    const string star;
+    const string name;
+
+    template <class Archive>
+    void serialize(Archive &ar) {
+        ar.appendAttribute("star", star.data());
+        ar.appendAttribute("name", name.data());
+    }
+};
+struct Director {
+    const string name;
+
+    template <class Archive>
+    void serialize(Archive &ar) {
+        ar.appendAttribute("name", name.data());
+    }
+};
+struct Writer {
+    const string name;
+
+    template <class Archive>
+    void serialize(Archive &ar) {
+        ar.appendAttribute("name", name.data());
+    }
+};
+
 struct Movie {
-    unsigned int id;
-    string title;
-    unsigned int year;
-    unsigned int length;
-    vector<string> directors;
-    vector<string> writers;
-    vector<string> cast;
-    tuple<string, string> castingRole;
+    const unsigned int id;
+    const string title;
+    const unsigned int year;
+    const unsigned int length;
+    vector<Role> cast;
+    vector<Director> directors;
+    vector<Writer> writers;
 
     template <class Archive>
     void serialize(Archive &ar) {
@@ -25,24 +54,39 @@ struct Movie {
         ar.appendAttribute("year", to_string(year).data());
         ar.appendAttribute("length", to_string(length).data());
         ar(
-                CEREAL_NVP(directors)
+                CEREAL_NVP(cast)
+                , CEREAL_NVP(directors)
                 , CEREAL_NVP(writers)
-                , CEREAL_NVP(cast)
-                , CEREAL_NVP(castingRole)
                 );
     }
 
 };
 
 int main() {
-    vector<string> directors = {"satochi", "tsubasa"};
-    vector<string> writers = {"aan", "tada"};
-    vector<string> casts = {"deka"};
-    tuple<string, string> castingRole = {make_tuple("ore", "torasan")};
-    Movie movie{1, "Pokemon", 1999, 200, directors, writers, casts, castingRole};
+    Director kon{name: "Kon Ichikawa"};
+    Director yoji{name: "Yoji Yamada"};
+    vector<Director> directors;
+    directors.push_back(kon);
+    directors.push_back(yoji);
+
+    Writer koki{name: "Koki Mitani"};
+    vector<Writer> writers;
+    writers.push_back(koki);
+
+    Role hiroki{star: "Hiroki Hasegawa", name: "Rando Yaguchi"};
+    Role satomi{star: "Satomi Ishihara", name: "Koyoko Ann Paterson"};
+    vector<Role> cast;
+    cast.push_back(hiroki);
+    cast.push_back(satomi);
+
+    Movie movie{1, "Pokemon", 1999, 200, cast, directors, writers};
     stringstream ss;
     {
-        cereal::XMLOutputArchive archive(ss);
+//        cereal::JSONOutputArchive archive(ss);
+        cereal::XMLOutputArchive::Options option = cereal::XMLOutputArchive::Options::Default();
+        option.sizeAttributes(false);
+        cereal::XMLOutputArchive archive(ss, option);
+
         archive(cereal::make_nvp("movie", movie));
     }
     cout << ss.str() << endl;
