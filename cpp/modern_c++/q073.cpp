7574b1e5dcc05ffccbdd75a5d0e9ee4172b0a036
WATANAPEI <info@wpei.dev>
2019-12-14 23:01:01 +0900

    didn't finish q073

--- cpp/modern_c++/q073.cpp
+++ cpp/modern_c++/q073.cpp
@@ -2,6 +2,7 @@
 #include <cereal/archives/xml.hpp>
 #include <cereal/archives/json.hpp>
 #include <vector>
+#include <fstream>
 #include <tuple>
 #include <sstream>
 #include <cereal/types/vector.hpp>
@@ -15,26 +16,34 @@ struct Role {
     const string star;
     const string name;
 
-    template <class Archive>
-    void serialize(Archive &ar) {
+    void save(cereal::XMLOutputArchive &ar) {
         ar.appendAttribute("star", star.data());
         ar.appendAttribute("name", name.data());
+    }
+
+    void load(cereal::XMLInputArchive &ar) {
+        ar(star,name);
     }
 };
 struct Director {
     const string name;
 
-    template <class Archive>
-    void serialize(Archive &ar) {
+    void save(cereal::XMLOutputArchive &ar) {
         ar.appendAttribute("name", name.data());
+    }
+
+    void load(cereal::XMLInputArchive &ar) {
+        ar(name);
     }
 };
 struct Writer {
     const string name;
 
-    template <class Archive>
-    void serialize(Archive &ar) {
+    void save(cereal::XMLOutputArchive &ar) {
         ar.appendAttribute("name", name.data());
+    }
+    void load(cereal::XMLInputArchive &ar) {
+        ar(name);
     }
 };
 
@@ -47,8 +56,7 @@ struct Movie {
     vector<Director> directors;
     vector<Writer> writers;
 
-    template <class Archive>
-    void serialize(Archive &ar) {
+    void save(cereal::XMLOutputArchive &ar) {
         ar.appendAttribute("id", to_string(id).data());
         ar.appendAttribute("title", title.data());
         ar.appendAttribute("year", to_string(year).data());
@@ -60,35 +68,50 @@ struct Movie {
                 );
     }
 
+    void load(cereal::XMLInputArchive &ari) {
+        ari(directors);
+    }
+
 };
 
 int main() {
-    Director kon{name: "Kon Ichikawa"};
-    Director yoji{name: "Yoji Yamada"};
-    vector<Director> directors;
-    directors.push_back(kon);
-    directors.push_back(yoji);
-
-    Writer koki{name: "Koki Mitani"};
-    vector<Writer> writers;
-    writers.push_back(koki);
-
-    Role hiroki{star: "Hiroki Hasegawa", name: "Rando Yaguchi"};
-    Role satomi{star: "Satomi Ishihara", name: "Koyoko Ann Paterson"};
-    vector<Role> cast;
-    cast.push_back(hiroki);
-    cast.push_back(satomi);
-
-    Movie movie{1, "Pokemon", 1999, 200, cast, directors, writers};
-    stringstream ss;
     {
+        Director kon{name: "Kon Ichikawa"};
+        Director yoji{name: "Yoji Yamada"};
+        vector<Director> directors;
+        directors.push_back(kon);
+        directors.push_back(yoji);
+
+        Writer koki{name: "Koki Mitani"};
+        vector<Writer> writers;
+        writers.push_back(koki);
+
+        Role hiroki{star: "Hiroki Hasegawa", name: "Rando Yaguchi"};
+        Role satomi{star: "Satomi Ishihara", name: "Koyoko Ann Paterson"};
+        vector<Role> cast;
+        cast.push_back(hiroki);
+        cast.push_back(satomi);
+
+        Movie movie{1, "Pokemon", 1999, 200, cast, directors, writers};
+        stringstream ss;
 //        cereal::JSONOutputArchive archive(ss);
         cereal::XMLOutputArchive::Options option = cereal::XMLOutputArchive::Options::Default();
         option.sizeAttributes(false);
         cereal::XMLOutputArchive archive(ss, option);
 
         archive(cereal::make_nvp("movie", movie));
+        cout << ss.str() << endl;
     }
-    cout << ss.str() << endl;
+
+    {
+        vector<Director> directors;
+        string xmlPath = "/home/baystars/string/learning/cpp/modern_c++/q073.xml";
+        std::ifstream is(xmlPath);
+        cereal::XMLInputArchive ari(is);
+        ari(directors);
+        cout << directors.at(0).name << endl;
+
+    }
+
 
 }
