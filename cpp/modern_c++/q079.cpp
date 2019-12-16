80de73392174e41eaa34dfe624bfb823a51a5fd7
WATANAPEI <info@wpei.dev>
2019-12-16 21:26:38 +0900

    solved question 79

--- cpp/modern_c++/q079.cpp
+++ cpp/modern_c++/q079.cpp
@@ -6,18 +6,43 @@
 #include <crtdbg.h>
 #endif
 #include <iostream>
+#include <vector>
 #include <regex>
 #include <ZipLib/ZipFile.h>
 #include <ZipLib/ZipArchive.h>
 #include <fstream>
+#include <experimental/filesystem>
 
 using namespace std;
-
+namespace fs = experimental::filesystem;
+static const string zipFileName = "c.zip";
 
 int main() {
-    string zipFileName = "c.zip";
+    regex re;
+    string regexString;
+    cout << "input file pattern: " << endl;
+    cin >> regexString;
+    try{
+        regex regexTemp(regexString);
+        re = regexTemp;
+    } catch(regex_error &e) {
+        cout << e.code() << endl;
+        return -1;
+    }
+    cout << "regexString: " << regexString << endl;
+
+    vector<ZipArchiveEntry> filteredFiles;
+
     auto archive = ZipFile::Open(zipFileName);
     size_t entries = archive->GetEntriesCount();
-    cout << "entries count: " << entries << endl;
+    for(size_t i = 0; i < entries; i++) {
+        auto entry = archive->GetEntry(int(i));
+        string entryName = entry->GetFullName();
+        //cout << entryName << endl;
+        if(regex_match(entryName, re)) {
+            cout << "matched: " << entryName << endl;
+        }
+    }
+    //cout << "entries count: " << entries << endl;
 
 }
