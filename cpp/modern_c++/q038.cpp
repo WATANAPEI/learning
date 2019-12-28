9fc4dc339de21aa45675462a97fd86a641e4ae9d
WATANAPEI <info@wpei.dev>
2019-12-28 15:12:19 +0900

    solve q038

--- cpp/modern_c++/q038.cpp
+++ cpp/modern_c++/q038.cpp
@@ -1,43 +1,63 @@
+//use compile command below
+// g++ -std=c++1z filename -lstdc++fs
+
 #include <iostream>
+#include <memory>
+#include <fstream>
 #include <experimental/filesystem>
+#include <date/date.h>
+#include <chrono>
+
 
 using namespace std;
-namespace fs = std::experimental::filesystem;
+using namespace date;
+namespace fs = experimental::filesystem;
 
 class MyLog {
+private:
+    date::sys_seconds now;
+    string filename;
+    fs::path tmpPath;
+    ofstream output;
 public:
     MyLog()
-     : ostr(std::cout){}
-    explicit MyLog(ostream &ostr)
-        : ostr(ostr) {}
+    : now(date::floor<chrono::seconds>(chrono::system_clock::now())),
+        filename(string(date::format("%Y%m%d%H%M%S", now).append(".log"))),
+                tmpPath(filename), output(tmpPath) {}
+    explicit MyLog(const string &str)
+    : now(date::floor<chrono::seconds>(chrono::system_clock::now())),
+        filename(string(str).append(".log")),
+                tmpPath(filename), output(tmpPath) {}
+    void append(const string &str) {
+        output << str;
+    }
+    void save(string const &str) {
+        fs::path persistPath(str);
+        fs::path persistDirectory = persistPath.parent_path();
+        fs::create_directories(persistDirectory);
+        output.flush();
+        fs::copy_file(tmpPath, persistPath, fs::copy_options::overwrite_existing);
+    }
+    ~MyLog() {
+        output.flush();
+        fs::remove(filename);
+    }
 
-
-private:
-    ostream &ostr;
 };
 
+
 int main() {
-    string str("test log");
     {
-        try{
-            MyLog log;
-        } catch (MyLogError e) {
-            cout << e.what() << endl;
-        };
+        string str("test log");
+        MyLog log;
         log.append(str);
         log.append("aaaa");
+        log.save("tmp/test1.log");
+        log.append("bbb");
     }
-    {
-        MyLog log2;
-        Path path("./log.txt");
-        try{
-            log2.create(path);
-        } catch (MyLogError e) {
-            cout << e.what();
-        }
-        log2.append("sfagdeag\n");
-        log2.append("ihiuuuu");
-        log2.save();
-    }
+    MyLog log2("test2.txt");
+    log2.append("sfagdeag\n");
+    log2.append("ihiuuuu");
+    //log2.save("tmp/test2.log");
 
 }
