5739f447ebf0a789885376d0c7b7400679529112
WATANAPEI <info@wpei.dev>
2019-12-29 10:40:24 +0900

    made cmakelist

--- cpp/modern_c++/q043/q043.cpp
+++ cpp/modern_c++/q043/q043.cpp
@@ -1,5 +1,7 @@
 //compile command
-//g++ -std=c++1z filename -l /usr/local/include
+// g++ -std=c++1z -c q043.cpp /usr/local/src/date/src/tz.cpp -I/usr/local/src/date/include/date/ -I/usr/local/src/curl/include
+// g++ -o q043 q043.o tz.o -L/usr/lib/x86_64-linux-gnu -lcurl
+// also, check ldconfig whether it's latest version or not
 #include <iostream>
 #include <chrono>
 #include "date.h"
@@ -10,5 +12,5 @@ using namespace std;
 int main() {
 
     auto t = date::make_zoned(date::current_zone(), chrono::system_clock::now());
-    //cout << t << "\n";
+    cout << t << "\n";
 }
