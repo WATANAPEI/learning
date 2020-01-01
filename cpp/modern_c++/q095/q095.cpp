e01e4ac47d2b37aa8ab37ee05854e33269a4f2d3
WATANAPEI <info@wpei.dev>
2020-01-01 17:45:17 +0900

    copied answer for q95

--- cpp/modern_c++/q095/q095.cpp
+++ cpp/modern_c++/q095/q095.cpp
@@ -1,10 +1,31 @@
 #define ASIO_STANDALONE
 #include <iostream>
 #include "asio.hpp"
+#include <vector>
 
 using namespace std;
 
-int main() {
-    cout << "test" << endl;
+vector<string> get_ip_address(string const &hostname)
+{
+    vector<string> ips;
 
+    try{
+        asio::io_context context;
+        asio::ip::tcp::resolver resolver(context);
+        auto endpoints = resolver.resolve(asio::ip::tcp::v4(), hostname.c_str(), "");
+
+        for(auto const &e: endpoints) {
+            ips.push_back(e.endpoint().address().to_string());
+        }
+    } catch(exception const&e) {
+        cerr << "exception: " << e.what() << endl;
+    }
+
+    return ips;
+}
+
+int main() {
+    auto ips = get_ip_address("packtpub.com");
+    for(auto const &ip: ips)
+        cout << ip << endl;
 }
