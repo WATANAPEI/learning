e4bb729463fb972939c5599d6fcc025066a82faf
WATANAPEI <info@wpei.dev>
2020-01-03 23:07:06 +0900

    temp save

--- cpp/modern_c++/q065/q065.cpp
+++ cpp/modern_c++/q065/q065.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
-
+#include <sstream>
 
 using namespace std;
 
 class PLog {
-public:
-    static void write(string msg) {
-        cout << "test" << endl;
+    mutex mtx;
 
+public:
+    unique_lock<mutex> get_lock()
+    {
+        return unique_lock<mutex>(mtx);
     }
 
+    void log(string msg) {
+        unique_lock<mutex> lk = get_lock();
+        cout << msg << endl;
+    }
 };
 
 int main() {
-    string msg("test message");
-    auto thread1 = std::thread([&]() {PLog::write(msg);});
-    auto thread2 = std::thread([&]() {PLog::write(msg);});
+    PLog p;
+    stringstream sstr1;
+    stringstream sstr2;
+    auto thread1 = std::thread([&]() {
+            for(int i = 0;i < 10; i++) {
+                sstr1 << "thread1 i: " << i << endl;
+                p.log(sstr1.str());
+            }});
+    auto thread2 = std::thread([&]() {
+            for(int i = 0;i < 10; i++) {
+                sstr2 << "thread2 i: " << i << endl;
+                p.log(sstr2.str());
+            }});
     thread1.join();
     thread2.join();
 
