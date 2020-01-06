5b1e1c956e2ef5738559ca5c1b1aea7d8393e035
WATANAPEI <info@wpei.dev>
2020-01-06 23:48:06 +0900

    could show hello world in pdf file

--- cpp/modern_c++/q077/q077.cpp
+++ cpp/modern_c++/q077/q077.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <PDFWriter/PDFWriter.h>
+#include <PDFWriter/PDFPage.h>
+#include <PDFWriter/PageContentContext.h>
+#include <PDFWriter/AbstractContentContext.h>
+
 
 using namespace std;
 
 int main() {
     PDFWriter pdfwriter;
-    pdfwriter.StartPDF("./test.pdf", ePDFVersion13);
+    pdfwriter.StartPDF("/mnt/c/Users/Baystars/Downloads/test.pdf", ePDFVersion13);
+    PDFPage *page = new PDFPage();
+    page->SetMediaBox(PDFRectangle(0, 0, 595, 842));
+    PageContentContext *contentContext = pdfwriter.StartPageContentContext(page);
+    PDFUsedFont *font = pdfwriter.GetFontForFile("/usr/local/src/pngwriter/fonts/FreeMonoBold.ttf");
+    AbstractContentContext::TextOptions textOptions(font, 14, AbstractContentContext::eGray, 0);
+    contentContext->WriteText(10, 100, "hello world", textOptions);
+    pdfwriter.EndPageContentContext(contentContext);
+    pdfwriter.WritePageAndRelease(page);
     pdfwriter.EndPDF();
-    cout << "test" << endl;
+    cout << "Finished" << endl;
 
 }
