b280fd3326de58d15458a03b8ba71d0b890b97c0
WATANAPEI <info@wpei.dev>
2020-01-11 16:52:43 +0900

    add decryption

--- cpp/modern_c++/q089/q089.cpp
+++ cpp/modern_c++/q089/q089.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-string vigenereCipher(string const& in, string const& key) {
+string vigenereEncrypt(string const& in, string const& key) {
     string result;
     size_t strLength = key.size();
     for(size_t i = 0; i < in.size(); ++i) {
@@ -13,13 +13,29 @@ string vigenereCipher(string const& in, string const& key) {
     return result;
 }
 
+string vigenereDecrypt(string const& in, string const& key) {
+    string result;
+    size_t strLength = key.size();
+    for(size_t i = 0; i < in.size(); ++i) {
+        if(in.at(i) - key.at(i % strLength) < 0) {
+            result.push_back(static_cast<char>(((in.at(i) - 'A') - (key.at(i % strLength) - 'A')) + 26 + 'A'));
+        } else {
+            result.push_back(static_cast<char>(((in.at(i) - 'A') - (key.at(i % strLength) - 'A')) % 26 + 'A'));
+        }
+    }
+    return result;
+}
 int main() {
-    string in;
+    string phrase;
     cout << "input string:";
-    cin >> in;
+    cin >> phrase;
     string key("ARM");
-    string out = vigenereCipher(in, key);
-    cout << "in: " << in << endl;
-    cout << "out: " << out << endl;
-    assert(vigenereCipher("CODE", "ARM") == "CFPE");
+    string code = vigenereEncrypt(phrase, key);
+    cout << "phrase: " << phrase << endl;
+    cout << "code: " << code << endl;
+    string decodedPhrase = vigenereDecrypt(code, key);
+    cout << "decodedPhrase: " << decodedPhrase << endl;
+    assert(vigenereEncrypt("CODE", "ARM") == "CFPE");
+    assert(vigenereDecrypt("CFPE", "ARM") == "CODE");
+
 }
