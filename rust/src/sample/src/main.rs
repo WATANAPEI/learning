5c691332bae9279f7d48c6d50c10d80f286d9c81
WATANAPEI <info@wpei.dev>
2020-02-09 11:31:38 +0900

    add some samples

--- rust/src/sample/src/main.rs
+++ rust/src/sample/src/main.rs
@@ -1,39 +1,15 @@
-#[derive(Debug)]
-
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quater from {:?}", state);
-            25
-        }
-    }
-}
-
-fn plus_one(x: Option<i32> ) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
+use std::collections::HashMap;
 
 fn main() {
-    let five: Option<i32> = Some(5);
-    let x = plus_one(five);
-    let none = plus_one(None);
+    let text = "hello world wonderful world";
 
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
+
 
