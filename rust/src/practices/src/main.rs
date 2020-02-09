3ecc3d198e4f15c362aebea4e3e11f9d5ed846cf
WATANAPEI <info@wpei.dev>
2020-02-09 12:01:13 +0900

    add practice

---
+++ rust/src/practices/src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut v: Vec<isize> = vec![2, 5, 20, -3, 4, 5];
+    let mut sum = 0;
+    for i in &v {
+        sum += *i;
+    }
+    let len = v.len();
+    let avg = sum / (len as isize);
+    println!("average of v is: {}", avg);
+    v.sort();
+    println!("median of v is: {}", v[len/2]);
+    let mut map = HashMap::new();
+    for i in &v {
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+
+
+
+}
