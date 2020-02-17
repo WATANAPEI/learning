6fcfef8c9037454cae96bb659a2a3ab26c24c05b
WATANAPEI <info@wpei.dev>
2020-02-18 00:00:34 +0900

    add samples

--- rust/src/sample/src/main.rs
+++ rust/src/sample/src/main.rs
@@ -1,89 +1,36 @@
-use std::thread;
-use std::time::Duration;
-use std::collections::HashMap;
-
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
 }
 
-fn main() {
-    let simulated_user_specified_value = 100;
-    let simulated_random_number = 7;
-
-    generate_workout (
-        simulated_user_specified_value,
-        simulated_random_number
-        );
-}
-
-fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_result = Cacher::new( |num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            expensive_result.value(intensity)
-            );
-        println!("Next, do {} situps!",
-                 expensive_result.value(intensity)
-            );
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay haydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_result.value(intensity)
-                );
-        }
-    }
-}
-
-struct Cacher<T>
-    where T: Fn(u32) -> u32
-{
-    calculation: T,
-    value: HashMap<u32, u32>,
-}
-
-impl<T> Cacher<T>
-    where T: Fn(u32) -> u32
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: HashMap::new(),
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value.get(&arg) {
-            Some(&v) => {
-                v
-            },
-            None => {
-                let result = (self.calculation)(arg);
-                self.value.insert(arg, result);
-                result
-            },
-        }
-    }
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size)
+        .collect()
 }
 
 #[test]
-fn call_with_different_values() {
-    let mut c = Cacher::new(|a| a);
+fn filters_by_size() {
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker")},
+        Shoe { size: 13, style: String::from("sandal")},
+        Shoe { size: 10, style: String::from("boot")},
+    ];
 
-    let v1 = c.value(1);
-    let v2 = c.value(2);
+    let in_my_size = shoes_in_my_size(shoes, 10);
 
-    assert_eq!(v2, 2);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 10, style: String::from("boot") },
+        ]
+        );
 }
 
 
+
+fn main() {
+
+}
