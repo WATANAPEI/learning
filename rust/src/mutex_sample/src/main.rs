5e40c89e911f796ef628670ed98d11e6c68accd8
WATANAPEI <info@wpei.dev>
2020-02-23 23:16:52 +0900

    add deadlock sample

--- rust/src/mutex_sample/src/main.rs
+++ rust/src/mutex_sample/src/main.rs
@@ -2,22 +2,27 @@ use std::sync::{ Mutex, Arc };
 use std::thread;
 
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
+    let counter1_1 = Arc::new(Mutex::new(0));
+    let d1 = counter1_1.lock();
+    let d2 = counter1_1.lock();
+    /*
+    let counter2_1 = Arc::new(Mutex::new(1));
 
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
 
-            *num += 1;
-        });
-        handles.push(handle);
-    }
+    let mut handle1 = counter1_1.lock().unwrap();
+    *handle1 += 1;
+    let counter1_2 = Arc::clone(&counter1_1);
+    let counter2_2 = Arc::clone(&counter2_1);
+    thread::spawn(move || {
+        let mut handle2_1 = counter2_1.lock().unwrap();
+        *handle2_1 += 1;
+        let mut handle1_2 = counter1_2.lock().unwrap();
+        *handle1_2 += 1;
+    });
+    let mut handle2_2 = counter2_2.lock().unwrap();
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
 
-    println!("Result: {}", *counter.lock().unwrap());
+    println!("Result: {}", *handle1);
+    println!("Result: {}", *handle2_2);
+    */
 }
