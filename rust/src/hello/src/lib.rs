7765872527244d85b1d01fb043cc46496a3f47f9
WATANAPEI <info@wpei.dev>
2020-03-07 07:43:49 +0900

    add samples

--- rust/src/hello/src/lib.rs
+++ rust/src/hello/src/lib.rs
@@ -1,8 +1,34 @@
 pub struct ThreadPool;
+//use std::error;
+//use std::fmt;
+
+enum PoolCreationError {
+    ThreadCreation,
+}
+
+/* UNdone
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            PoolCreationError::ThreadCreation
+            */
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+    /// 新しいThreadPoolを生成する。
+    ///
+    /// sizeがプールのスレッド数です。
+    /// # panic
+    ///
+    /// sizeが0なら、`new`関数はパニックします。
+    ///
+    /// Create a new ThreadPool.
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
+
+q
+qq
+:q
+:q
+
     }
 
     pub fn execute<F>(&self, f: F)
