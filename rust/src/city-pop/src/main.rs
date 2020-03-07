c0eaff1c6ac58afa126a4616dcbb6425583bab0c
WATANAPEI <info@wpei.dev>
2020-03-07 09:01:59 +0900

    add error hanbdling sample

---
+++ rust/src/city-pop/src/main.rs
@@ -0,0 +1,59 @@
+extern crate getopts;
+extern crate rustc_serialize;
+extern crate csv;
+
+use std::env;
+use getopts::Options;
+
+#[derive(Debug, RustcDecodable)]
+struct Row {
+    country: String,
+    city: String,
+    accent_city: String,
+    region: String,
+
+    populatin: Option<u64>,
+    latitude: Option<u64>,
+    longtitude: Option<u64>,
+
+}
+
+fn print_usage(program: &str, opts: Options) {
+    println!("{}", opts.usage(&format!("Usage: {} [options] <data-path> <city>", program)));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Show this usage message.");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(e) => { panic!(e.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let data_file = args[1].clone();
+    let data_path = std::path::Path::new(&data_file);
+    let city = args[2].clone();
+
+    let file = std::fs::File::open(data_path).unwrap();
+    let mut rdr = csv::Reader::from_reader(file);
+
+    for row in rdr.decode::<Row>() {
+        let row = row.unwrap();
+
+        if row.city == city {
+            println!("{}, {}: {:?}", row.city, row.country, row.populatin.expect("populatin count"));
+        }
+    }
+
+
+
+
+}
