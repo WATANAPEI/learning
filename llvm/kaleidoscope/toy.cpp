2de2c97fb3daba84a6d8058ce7c2924988b50e37
WATANAPEI <info@wpei.dev>
2020-04-17 23:19:04 +0900

    add AST classes

--- llvm/kaleidoscope/toy.cpp
+++ llvm/kaleidoscope/toy.cpp
@@ -76,9 +76,70 @@ static int gettok() {
 }
 
 
+//==--------------------------------------------------------------------===//
+//Abstract Synctax Tree
+//==--------------------------------------------------------------------===//
 
+namespace {
 
+class ExprAST {
+public:
+    virtual ~ExprAST() = default;
+};
 
+class NumberExprAST : ExprAST {
+    double Val;
 
+public:
+    NumberExprAST(double Val) : Val(Val) {}
+};
 
+public VariableExprAST : public ExprAST {
+    std::string Name;
 
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+class BinaryExprAST : public ExprAST {
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+            std::unique_ptr<ExprAST> RHS)
+        : Op(Op), LHS(std::move(LHS), RHS(std::move(RHS)) {}
+};
+
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+    CallExprAST(const std::string &Callee,
+            std::vector<std::unique_ptr<ExprAST>> Args)
+    : Callee(Callee), Args(std::move(Args)) {}
+};
+
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+        : Name(Name), Args(std::move(Args)) {}
+
+    const std::string &getName() { return Name; }
+};
+
+class FunctionAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+            std::unique_ptr<ExprAST> Body)
+        : Proto(std::move(Proto)), std::Body(std::move(Body)) {}
+};
+
+}
