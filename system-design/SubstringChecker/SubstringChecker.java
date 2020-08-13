2f4673f4101850b01dd352c81075256cc8e95cb2
WATANAPEI <info@wpei.dev>
2020-08-13 09:24:51 +0900

    add java implementation

--- system-design/SubstringChecker/SubstringChecker.java
+++ system-design/SubstringChecker/SubstringChecker.java
@@ -16,14 +16,17 @@ public class SubstringChecker {
      * @return boolean
      */
     public static boolean is_substring(String str, String pattern) {
-        return bruteForceImpl(str, pattern);
+        //return bruteForceImpl(str, pattern);
+        return javaImpl(str, pattern);
     }
 
     private static boolean bruteForceImpl(String str, String pattern) {
         boolean foundMismatch;
-        for(int i = 0; i < str.length(); i++){
+        int processCount = 0;
+        for(int i = 0; i < str.length()-pattern.length()+1; i++){
             foundMismatch = false;
             for(int j = 0 ; j < pattern.length(); j++) {
+                processCount++;
                 if(str.charAt(i+j) != pattern.charAt(j)) {
                     foundMismatch = true;
                     break;
@@ -32,8 +35,51 @@ public class SubstringChecker {
             if(foundMismatch) {
                 continue;
             }
+            System.out.println("count: " + processCount);
             return true;
         }
+        System.out.println("count: " + processCount);
+        return false;
+    }
+
+    /**
+     * java implementation(indexOf)
+     * http://hg.openjdk.java.net/jdk7u/jdk7u6/jdk/file/8c2c5d63a17e/src/share/classes/java/lang/String.java#l1715
+     * @param str
+     * @param pattern
+     * @return
+     */
+    private static boolean javaImpl(String str, String pattern) {
+        char[] strArr = str.toCharArray();
+        char[] ptnArr = pattern.toCharArray();
+        char first = ptnArr[0];
+        int max = 0 + str.length() - pattern.length();
+        int processCount = 0;
+
+        for(int i = 0; i <= max; i++) {
+            // Look for first character
+            if(strArr[i] != first) {
+                while(++i <= max && strArr[i] != first) {
+                    processCount++;
+                };
+            }
+
+            //Found first character, now look at the rest of v2
+            if(i <= max) {
+                int j = i+1;
+                int end = j + pattern.length() - 1;
+                for( int k = 1; j < end && strArr[j] == ptnArr[k]; j++, k++) {
+                    processCount++;
+                };
+
+                if(j==end) {
+                    //Found whole string
+                    System.out.println("count: " + processCount);
+                    return true;
+                }
+            }
+        }
+        System.out.println("count: " + processCount);
         return false;
     }
 
