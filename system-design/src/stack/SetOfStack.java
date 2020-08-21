5de0b0ab5deef88b4cb0635757c5607608d147de
WATANAPEI <info@wpei.dev>
2020-08-21 23:02:42 +0900

    implemented set of stack

--- system-design/src/stack/SetOfStack.java
+++ system-design/src/stack/SetOfStack.java
@@ -4,47 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SetOfStack {
-    List<Stack> s;
-    int maxCapacity = 20;
-    int set = 2;
+    List<Stack> stackList;
+    int maxCapacity = 10;
+    int set = 1;
     int currentSet;
 
     public SetOfStack() {
-        s = new ArrayList();
+        stackList = new ArrayList();
         for(int i = 0;i < set; i++) {
-            s.add(new Stack(maxCapacity));
+            stackList.add(new Stack(maxCapacity));
         }
         currentSet = 0;
     }
 
     public SetOfStack(int capacity, int set) {
         this.maxCapacity = capacity;
-        s = new ArrayList(set);
+        stackList = new ArrayList(set);
         for(int i =0;i < set; i++) {
-            s.add(new Stack(maxCapacity));
+            stackList.add(new Stack(maxCapacity));
         }
         currentSet = 0;
     }
 
     public void push(int value) {
-        if (s.get(currentSet).isCapable()) {
-            s.get(currentSet).push(value);
+        if (stackList.get(currentSet).isCapable()) {
+            stackList.get(currentSet).push(value);
             return;
         }
-        s.add(new Stack(s.get(0).getStackLength()));
-        s.get(s.size()-1).push(value);
+        stackList.add(new Stack(stackList.get(0).getStackLength()));
+        stackList.get(stackList.size()-1).push(value);
         currentSet++;
     }
 
     public int pop() {
-        return 0;
+        if(stackList.get(currentSet).isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        int result = stackList.get(currentSet).pop();
+        if(stackList.get(currentSet).isEmpty() && getStackNumber() != 1) {
+            stackList.remove(currentSet);
+            currentSet--;
+        }
+        return result;
     }
 
     public int getStackNumber() {
-        return s.size();
+        return stackList.size();
     }
 
     private boolean isEmpty(int setNumber) {
-        return s.get(setNumber).isEmpty();
+        return stackList.get(setNumber).isEmpty();
     }
 }
--- system-design/test/stack/SetOfStackTest.java
+++ system-design/test/stack/SetOfStackTest.java
@@ -17,12 +17,25 @@ class SetOfStackTest {
     @Test
     public void testStackPush() {
         SetOfStack s = new SetOfStack();
+        assertThrows(IllegalStateException.class, () -> {
+            s.pop();
+        });
         s.push(3);
         assertEquals(3, s.pop());
         s.push(4);
         s.push(5);
         assertEquals(5, s.pop());
 
+    }
+
+    @Test
+    public void testStackMany() {
+        SetOfStack s = new SetOfStack();
+        for(int i = 0; i < 20; i++) {
+            s.push(i);
+        }
+        assertEquals(19, s.pop());
+        assertEquals(2, s.getStackNumber());
     }
 
 }
