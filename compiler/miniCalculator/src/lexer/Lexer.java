54969a1b15f3e340db6eacd8af801a74bebc4442
WATANAPEI <info@wpei.dev>
2020-09-25 23:52:01 +0900

    implemented fake splitter

--- compiler/miniCalculator/src/lexer/Lexer.java
+++ compiler/miniCalculator/src/lexer/Lexer.java
@@ -1,26 +1,53 @@
 package lexer;
 
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 // TODO: Use Regex to separate tokens
 public class Lexer {
     Scanner sc;
-    String next;
     List<Token> tokenList;
+
     public Lexer(String str) {
         sc = new Scanner(str);
         sc.useDelimiter("[\\p{javaWhitespace}]+");
         tokenList = new ArrayList();
     }
-    public Lexer(Reader reader) {
-        sc = new Scanner(reader);
-        sc.useDelimiter("[\\p{javaWhitespace}]+");
+
+    public Lexer() {
         tokenList = new ArrayList();
     }
+
+    public List<String> splitString(String str) {
+        int currentTail = 0;
+        int currentPointer = 0;
+        List<String> result = new ArrayList();
+        EnumSet<TokenType> tokenTypes = EnumSet.allOf(TokenType.class);
+        while(currentPointer < str.length()) {
+            if(str.charAt(currentPointer) == ' ' || str.charAt(currentPointer) == '\t'
+                    || str.charAt(currentPointer) == '\n') {
+                currentTail++;
+                currentPointer++;
+                continue;
+            }
+            String subStr = str.substring(currentTail, currentPointer);
+            List<TokenType> matchedToken = tokenTypes.stream()
+                    .filter(t -> subStr.matches(t.getPattern()))
+                    .collect(Collectors.toList());
+            if(matchedToken.size() != 0) {
+                currentPointer++;
+                continue;
+            } else {
+                currentPointer--;
+                result.add(str.substring(currentTail, currentPointer));
+                currentTail = currentPointer;
+            }
+        }
+        return result;
+    }
+
     public List<Token> analyze() {
+        String next;
         while(sc.hasNext()) {
             next = sc.next();
             if(next.matches(TokenType.NUMBER.getPattern())) {
